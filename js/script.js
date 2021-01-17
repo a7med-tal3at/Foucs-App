7e121a8f7566d03d936187b4c3000f85bd859168
a7med-tal3at <astrafox159@gmail.com>
2021-01-17 20:23:49 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * JavaSript File
+ */
+
+// App Variables Definition
+
+let btn = document.querySelector(".btn");
+
+// App Functions
+
+function createInputDiv() {}
+
+// Actions
+
+btn.onclick = function () {
+  console.log("a");
+};
---
+++ outline.md
@@ -0,0 +1,1 @@
+there are 25m to lessining and 30m to applaying and 5m between them for rest
